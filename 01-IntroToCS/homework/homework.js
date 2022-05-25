6fb49a647fa601d451f638993371a4850cbde60c
Lorenz0velez <lorenzoveleznavarro@gmail.com>
2022-05-25 10:33:05 -0300

    Homework Clase 1

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,13 +2,26 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  let potenciaPos = numero.length - 1
+  for (let i = 0; i < numero.length; i++) {
+    resultado += numero[i] * Math.pow(2,potenciaPos);
+    potenciaPos --
+    console.log(resultado)
+  }
+  return resultado
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  let numero = num
+  let resultado = ""
+  while (numero > 0) {
+    resultado = numero % 2 + resultado
+    numero = Math.floor(numero/2)
+  }
+  return resultado
 }
+
 
 
 module.exports = {
